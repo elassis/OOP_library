d23166cc506df7e7450399dd6f5092a04784e80b
enmanuel <enmanuellassis@gmail.com>
2021-12-22 16:39:30 -0400

    add teacher and student testing files

---
+++ ___testing___/test_teacher_spec.rb
@@ -0,0 +1,10 @@
+require './teacher'
+
+describe 'testing teacher specialization' do
+  context 'test teacher class' do
+    it 'specialization should be equal to math' do
+      teacher = Teacher.new(age:25,name:"Jonas",specialization:"math",parent_permission:true)
+      expect(teacher.specialization).to eql "math" 
+    end  
+  end
+end
