eeec54074a0e72a0e360506dcb19a5d252ede3bd
nachosala <nachosala89@gmail.com>
2021-12-22 22:52:46 -0300

    Test Books, Person and Classroom classes

---
+++ spec/test_classroom_spec.rb
@@ -0,0 +1,6 @@
+require './classroom'
+
+describe Classroom.new("Classroom 2") do 
+  it { is_expected.to have_attributes(label: "Classroom 2") }
+  it { is_expected.to have_attributes(students: []) }
+end
